a8495209b24697acecd2541708c20e8cc82fe97d
Chethina2003 <kovida.fernando@gmail.com>
2026-04-17 22:49:19 +0530

    Updated the pom.xml with latest dependencies

--- smartcampusapi/src/main/java/com/example/smartcampusapi/SmartCampusApplication.java
+++ smartcampusapi/src/main/java/com/example/smartcampusapi/SmartCampusApplication.java
@@ -4,10 +4,9 @@
  */
 package com.example.smartcampusapi;
 
-/**
- *
- * @author O2
- */
-public class SmartCampusApplication {
-    
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application {
 }
