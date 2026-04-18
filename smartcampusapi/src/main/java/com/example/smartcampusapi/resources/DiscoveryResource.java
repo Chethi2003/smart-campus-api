5118f78e71934ffa6c5bdeb50417d98d53b97633
Chethina2003 <kovida.fernando@gmail.com>
2026-04-18 15:13:15 +0530

    Create DiscoveryResource.java

---
+++ smartcampusapi/src/main/java/com/example/smartcampusapi/resources/DiscoveryResource.java
@@ -0,0 +1,31 @@
+package com.example.smartcampusapi.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/")
+public class DiscoveryResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Object> getApiInfo() {
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("name", "Smart Campus API");
+        response.put("version", "v1");
+
+        Map<String, String> endpoints = new HashMap<>();
+        endpoints.put("test", "/api/v1/test");
+        endpoints.put("rooms", "/api/v1/rooms");
+        endpoints.put("sensors", "/api/v1/sensors");
+
+        response.put("endpoints", endpoints);
+
+        return response;
+    }
+}
